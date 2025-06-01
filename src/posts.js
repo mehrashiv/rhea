97101a554c690d508d5020a9fbfe88871cdbc4f6
Shiv Mehra <mehrashiv@yahoo.com>
2025-05-31 19:57:07 -0700

    skirt

--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,14 @@
 // posts.js - Blog posts data storage
 export const blogPosts = [
   {
+    id: 2,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8542.jpeg",
+    tags: ["Art"]
+  },
+  {
     id: 1,
     title: " DIY Garden House",
     date: "2024-11-18",
@@ -8,6 +16,4 @@ export const blogPosts = [
     image: "https://minio-api.rayanmehra.com/public/rhea/gardendiy.JPEG",
     tags: ["Art"]
   },
-  
-  
 ];
