29ca66c9c65ba13fc60a9d4f26f189c187528572
Shiv Mehra <mehrashiv@yahoo.com>
2025-06-14 16:06:27 -0700

    added my work from class

--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,54 @@
 // posts.js - Blog posts data storage
 export const blogPosts = [
   {
+    id: 8,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8597.jpeg",
+    tags: ["Fashion"]
+  },
+  {
+    id: 7,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8596.jpeg",
+    tags: ["Fashion"]
+  },
+  {
+    id: 6,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8595.jpeg",
+    tags: ["Fashion"]
+  },
+  {
+    id: 5,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8594.jpeg",
+    tags: ["Fashion"]
+  },
+  {
+    id: 4,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8593.jpeg",
+    tags: ["Fashion"]
+  },
+  {
+    id: 3,
+    title: "DIY denim mini skirt",
+    date: "2025-05-31",
+    content: "Me and my friend made this skirt out of my old pair of jeans. It fits us both and we are taking turns wearing it to school. I am so proud of this skirt!!!",
+    image: "https://minio-api.rayanmehra.com/public/rhea/IMG_8592.jpeg",
+    tags: ["Fashion"]
+  },
+  {
     id: 2,
     title: "DIY denim mini skirt",
     date: "2025-05-31",
